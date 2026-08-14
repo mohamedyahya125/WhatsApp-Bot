9a42e3b3f6536b3ea5443645d65bf33714f1563a
Mohamed Yahya <mohamedyehya1132004@gmail.com>
2026-08-14 15:35:11 +0300

    debug webhook

--- app/Http/Controllers/TelegramWebhookController.php
+++ app/Http/Controllers/TelegramWebhookController.php
@@ -1,24 +1,4 @@
-<?php
-
-namespace App\Http\Controllers;
-
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Http;
-
-class TelegramWebhookController extends Controller
+public function webhook(Request $request)
 {
-    public function webhook(Request $request)
-    {
-        $chatId = $request->input('message.chat.id');
-
-        Http::post(
-            'https://api.telegram.org/bot' . env('TELEGRAM_BOT_TOKEN') . '/sendMessage',
-            [
-                'chat_id' => $chatId,
-                'text' => 'Hello From Laravel',
-            ]
-        );
-
-        return response()->json(['ok' => true]);
-    }
+dd($request->all());
 }
