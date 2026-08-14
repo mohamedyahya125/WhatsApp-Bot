96ad20700973a8da2a37130d37d2a22df05ffa93
Mohamed Yahya <mohamedyehya1132004@gmail.com>
2026-08-14 15:23:10 +0300

    reset telegram bot

--- app/Http/Controllers/TelegramWebhookController.php
+++ app/Http/Controllers/TelegramWebhookController.php
@@ -3,16 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Http;
 
 class TelegramWebhookController extends Controller
 {
     public function webhook(Request $request)
     {
-        Log::info('Telegram Update Received', $request->all());
+        $chatId = $request->input('message.chat.id');
 
-        return response()->json([
-            'ok' => true
-        ]);
+        if ($chatId) {
+            Http::post(
+                'https://api.telegram.org/bot' . env('TELEGRAM_BOT_TOKEN') . '/sendMessage',
+                [
+                    'chat_id' => $chatId,
+                    'text' => 'البوت شغال ✅',
+                ]
+            );
+        }
+
+        return response()->json(['ok' => true]);
     }
 }
